dcbb14f3c32e7670b87749e395f104151d9ea816
Stepan <stsalamtat@gmail.com>
2023-04-19 18:42:10 +0300

    1. set pom; 2. set DispatcherSerwlet and SpringConfig; 3. connected DB

---
+++ src/main/java/ru/chemakin/library/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,60 @@
+package ru.chemakin.library.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.EnumSet;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    /** возвращает конфигурационные классы, которые будут использоваться для настройки ApplicationContext
+        в данном случае NULL, что значит, что настройка будет проводиться классом SpringConfig **/
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    /** возвращает конфигурационные классы, которые будут использоваться для настройки DispatcherServlet **/
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    /** определяет, какие URL будут обрабатываться диспетчером сервлетов Spring MVC.
+     *  В данном случает будет обрабатывать все URL **/
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    /** метод вызывается при запуске приложения **/
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerCharacterEncodingFilter(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    /** метод, который регистрирует фильтр HiddenHttpMethodFilter,
+     *  который позволяет использовать HTTP-методы PUT, PATCH и DELETE в браузерах,
+     *  которые их не поддерживают. **/
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
+
+
+    /** метод, который устанавливает кодироку UTF-8 **/
+    private void registerCharacterEncodingFilter(ServletContext aContext) {
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic characterEncoding = aContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
+    }
+}
---
+++ src/main/java/ru/chemakin/library/config/SpringConfig.java
@@ -0,0 +1,91 @@
+package ru.chemakin.library.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("ru.chemakin.library")
+@EnableWebMvc // включает конфигурацию Spring MVC и дает возможность использовать аннотации @Controller и @RequestMapping.
+@PropertySource("classpath:database.properties")
+public class SpringConfig implements WebMvcConfigurer {
+    private final ApplicationContext applicationContext;
+    private final Environment environment;
+
+    @Autowired
+    public SpringConfig(ApplicationContext applicationContext, Environment environment) {
+        this.applicationContext = applicationContext;
+        this.environment = environment;
+    }
+
+
+    /** создает и возвращает SpringResourceTemplateResolver,
+     *  который используется для разрешения шаблонов Thymeleaf. **/
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+
+
+    /** создает и возвращает SpringTemplateEngine,
+     *  который используется для создания представлений с использованием шаблонов Thymeleaf. **/
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+
+    /** настраивает ThymeleafViewResolver для разрешения представлений,
+     *  созданных с использованием Thymeleaf. **/
+    @Override
+    public void configureViewResolvers (ViewResolverRegistry registry){
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+
+        registry.viewResolver(resolver);
+    }
+
+
+    /** создает и возвращает источник данных JDBC, настоенный в пропертях. **/
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("drivers");
+        dataSource.setUsername(environment.getProperty("DB_username"));
+        dataSource.setPassword(environment.getProperty("password"));
+        dataSource.setUrl(environment.getProperty("url"));
+
+        return dataSource;
+    }
+
+
+    /** создает и возвращает JdbcTemplate,
+     *  который используется для выполнения запросов к базе данных. **/
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(dataSource());
+    }
+}
+
